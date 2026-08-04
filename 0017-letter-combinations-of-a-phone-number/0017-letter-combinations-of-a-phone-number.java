7d0917e02726b5196bbbe226ae92c972c5621276
SThirunarayan1990 <thiru.hitam.dev@gmail.com>
2026-08-04 18:52:27 +0530

    Time: 0 ms (100%), Space: 43.7 MB (38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -12,18 +12,21 @@ class Solution {
 
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList();
-        result.add("");
-        for (char digit : digits.toCharArray()) {
-            int index = digit - '2';
-            String letters = digitToLetters[index];
-            List<String> tempLetterCombo = new ArrayList();
-            for (String existingLetterCombo : result) {
-                for (char letter : letters.toCharArray()) {
-                    tempLetterCombo.add(existingLetterCombo + letter);
-                }
-                result = tempLetterCombo;
-            }
-        }
+        backtrack(0, new StringBuilder(), digits, result);
         return result;
+    }
+
+    public void backtrack(int index, StringBuilder existingCombo, String digits, List<String> result) {
+        if (index == digits.length()) {
+            result.add(existingCombo.toString());
+            return;
+        }
+        int digitIndex = digits.charAt(index) - '2';
+        String letters = digitToLetters[digitIndex];
+        for (char letter : letters.toCharArray()) {
+            existingCombo.append(letter);
+            backtrack(index + 1, existingCombo, digits, result);
+            existingCombo.deleteCharAt(existingCombo.length() - 1);
+        }
     }
 }
