a4eca658095ac3b57e8bbc99bc42041dd9888362
SThirunarayan1990 <thiru.hitam.dev@gmail.com>
2026-08-05 10:29:59 +0530

    Time: 6 ms (72.64%), Space: 45.2 MB (68.18%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,25 +1,32 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> res = new ArrayList();
-        backtrack(res, new ArrayList(), candidates, target, 0, 0);
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
     }
 
-    void backtrack(List<List<Integer>> res, List<Integer> curr, int[] candidates, int target, int sum, int index) {
-        if (target == sum) {
-            res.add(new ArrayList(curr));
+    private void backtrack(List<List<Integer>> result, List<Integer> curr, int[] candidates, int target, int start) {
+
+        if (target == 0) {
+            result.add(new ArrayList<>(curr));
             return;
         }
-        if (index == candidates.length || sum > target) {
-            return;
+
+        for (int i = start; i < candidates.length; i++) {
+            
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            curr.add(candidates[i]);
+            backtrack(result, curr, candidates, target - candidates[i], i + 1);
+            curr.remove(curr.size() - 1);
+            
         }
-        curr.add(candidates[index]);
-        backtrack(res, curr, candidates, target, sum + candidates[index], index + 1);
-        curr.remove(curr.size() - 1);
-        while (index + 1 < candidates.length && candidates[index] == candidates[index + 1]) {
-            index++;
-        }
-        backtrack(res, curr, candidates, target, sum, index + 1);
     }
 }
