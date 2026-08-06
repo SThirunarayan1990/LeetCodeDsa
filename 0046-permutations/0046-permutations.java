4c1e5e179246f669ebe4ac557b25e1376c6a0f80
SThirunarayan1990 <thiru.hitam.dev@gmail.com>
2026-08-06 10:52:44 +0530

    Time: 0 ms (100%), Space: 45.6 MB (46.29%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,19 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList());
-
-        for (int i : nums) {
-            List<List<Integer>> temp = new ArrayList<>();
-            for (List<Integer> existingNum : res) {
-                for (int j = 0; j <= existingNum.size(); j++) {
-                    List<Integer> newList = new ArrayList<>(existingNum);
-                    newList.add(j, i);
-                    temp.add(newList);
-                }
-            }
-            res = temp;
-        }
+        boolean visited[] = new boolean[nums.length];
+        backtrack(res, nums, 0);
         return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, int[] nums, int index) {
+        if (index == nums.length) {
+            List<Integer> ns = new ArrayList();
+            for (int n : nums) {
+                ns.add(n);
+            }
+            res.add(ns);
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, index, i);
+            backtrack(res, nums, index + 1);
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
