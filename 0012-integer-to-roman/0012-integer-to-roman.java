9e3fb355f5b22ee283fe6d7b9c492c9184122016
SThirunarayan1990 <thiru.hitam.dev@gmail.com>
2026-08-06 21:13:47 +0530

    Time: 3 ms (99.84%), Space: 46.3 MB (61.53%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String intToRoman(int num) {
+        int n[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String sym[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n.length; i++) {
+            while (num >= n[i]) {
+                num = num - n[i];
+                sb.append(sym[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
